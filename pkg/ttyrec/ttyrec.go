a4722cb6cb733f70cb551ef9d48e8dc5821a132d
Rebecca Pruim <me@rebeccapruim.com>
2020-05-19 18:33:44 -0700

    Remove dependency on syscall

--- pkg/ttyrec/ttyrec.go
+++ pkg/ttyrec/ttyrec.go
@@ -4,13 +4,26 @@ import (
 	"encoding/binary"
 	"os"
 	"sync"
-	"syscall"
 	"time"
 )
 
 type TTYRecorder struct {
 	wr  *os.File
 	mux sync.Mutex
+}
+
+type Timeval struct {
+	Sec  int32
+	Usec int32
+}
+
+// NanosToTimeval is roughly compatible with the NsecToTimeval syscall without a syscall
+func NanosToTimeval(ns int64) *Timeval {
+	tMicros := ns / 1000
+	return &Timeval{
+		Sec:  int32(tMicros / 1E6),
+		Usec: int32(tMicros % 1E6),
+	}
 }
 
 func NewTTYRecorder(fp string) (*TTYRecorder, error) {
@@ -30,13 +43,13 @@ func (tr *TTYRecorder) Close() error {
 
 func (tr *TTYRecorder) writeBytes(data []byte) (int, error) {
 	t := time.Now()
-	timeval := syscall.NsecToTimeval(t.UnixNano())
+	timeval := NanosToTimeval(t.UnixNano())
 	writeLen := len(data)
-	err := binary.Write(tr.wr, binary.LittleEndian, int32(timeval.Sec))
+	err := binary.Write(tr.wr, binary.LittleEndian, timeval.Sec)
 	if err != nil {
 		return -1, err
 	}
-	err = binary.Write(tr.wr, binary.LittleEndian, int32(timeval.Usec))
+	err = binary.Write(tr.wr, binary.LittleEndian, timeval.Usec)
 	if err != nil {
 		return -1, err
 	}
